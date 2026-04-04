e7d980e2732a9463095d53c6ba49f6bfa6d29670
emanalytic <emannisar777@gmail.com>
2026-04-05 04:49:38 +0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,13 +5,22 @@ def main():
     # TODO: Uncomment the code below to pass the first stage
     while True:
         sys.stdout.write("$ ")
-        command = str(input())
+        command = input()
         if command == "exit":
             break
         if command.startswith("echo"):
             print(command[5:])
+        if command.startswith("type"):
+            if command[5:] == "echo":
+                print("echo is a shell builtin")
+            elif command[5:] == "type":
+                print("type is a shell builtin")
+            elif command[5:] == "exit":
+                print("exit is a shell builtin")
+            else:
+                print(f"{command[5:]}: not found")
         else:
-            print(f"{command}: command not found")
+            print(f"{command}: not found")
 
 
 if __name__ == "__main__":
