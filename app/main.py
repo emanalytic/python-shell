3b16d21b7ec7d999f264141d632117d250663014
emanalytic <emannisar777@gmail.com>
2026-04-05 21:19:30 +0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
+from subprocess import check_call
 import sys
 import os
+import subprocess
+from pathlib import Path
 
-def resolve_cmd(cmd):
-    path_dirs = os.environ.get("PATH").split(":")
+def find_executable(cmd):
+    path_dirs = os.environ.get("PATH", "").split(";")
+    pathext = os.environ.get("PATHEXT", "").split(";")
+
+    ## if cmd already has extension, try directly
     for directory in path_dirs:
-        full_path = os.path.join(directory, cmd)
-        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-            return full_path
+        current_path = Path(directory) / cmd
+        if current_path.is_file():
+            return current_path
+
+        # try Windows extensions
+        for ext in pathext:
+            candidate = Path(directory) / (cmd + ext.lower())
+            if candidate.is_file():
+                return candidate
+
     return None
-
-
 
 def main():
     while True:
@@ -21,40 +32,45 @@ def main():
             break
         if command.strip() == "":
             continue
-        if command == "exit":
+        
+
+        argv = command.split()
+        cmd = argv[0]
+        args = argv[1:]
+
+        if cmd == "exit":
             break
 
+
         ## builtin commands
-        elif command.startswith("echo"):
+        if cmd == "echo":
             print(command[5:])
             continue
 
-        elif command.startswith("type"):
-            cmd = command[5:].strip()
-            if cmd in ("echo", "type", "exit"):
-                print(f"{cmd} is a shell builtin")
+        elif cmd == "type":
+            if not args:
+                print("type: missing argument")
+                continue
+            
+            if args[0] in ("echo", "type", "exit"):
+                print(f"{args[0]} is a shell builtin")
             else:
-                full_path = resolve_cmd(cmd)
+                full_path = find_executable(args[0])
                 if full_path:
                     print(f"{cmd} is {full_path}")
                 else:
                     print(f"{cmd}: not found")
             continue
         # external command execution 
-        parts = command.split()
-        cmd = parts[0]
-        args = parts[1:]
-        full_path = resolve_cmd(cmd)
-        if full_path is None:
-            print(f"{cmd}: not found")
-            continue
-        
-        pid = os.fork()
-        if pid == 0:
-            os.execv(full_path, [cmd] + args)
         else:
-            os.waitpid(pid, 0)
-
+            try:
+                exec_path = find_executable(cmd)
+                if exec_path:
+                    subprocess.run([exec_path] + args)
+                else:
+                    print(f"{cmd}: not found")
+            except Exception as e:
+                print(e)
 
 if __name__ == "__main__":
     main()
